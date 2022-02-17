40f2b096ba90eb28841af7da3337124af8f4d6e9
unluckyjung <ybook2012@gmail.com>
2022-02-18 00:33:48 +0900

    test: mockWebServer 와 WebClient 조합 기본테스트

---
+++ play-ground/src/test/java/com/example/playground/MockWebServerAndWebClientTest.java
@@ -0,0 +1,48 @@
+package com.example.playground;
+
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+
+
+@Slf4j
+class MockWebServerAndWebClientTest {
+
+    public static MockWebServer mockWebServer;
+
+    @BeforeAll
+    static void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start(9999);
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    void callMockSever() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://localhost:9999")
+                .build();
+
+        MockResponse mockResponse = new MockResponse()
+                .setResponseCode(HttpStatus.OK.value())
+                .setBody("this is YoonSung Body");
+
+        mockWebServer.enqueue(mockResponse);
+
+        Mono<String> responseBody = webClient.get().retrieve().bodyToMono(String.class);
+
+        System.out.println(responseBody.block());    // 결과 획득
+    }
+}
