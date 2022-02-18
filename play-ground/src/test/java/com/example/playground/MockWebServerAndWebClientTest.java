2731bea29c3d34d9714179c86186bf9993af04f5
unluckyjung <ybook2012@gmail.com>
2022-02-18 23:08:54 +0900

    test: retrieve()시 400,500번대 에러는 예외가 발생한다는것 확인

--- play-ground/src/test/java/com/example/playground/MockWebServerAndWebClientTest.java
+++ play-ground/src/test/java/com/example/playground/MockWebServerAndWebClientTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
@@ -48,4 +49,60 @@ class MockWebServerAndWebClientTest {
 
         System.out.println(responseBody.block());    // 결과 획득
     }
+
+    @Test
+    void callMockSeverFail() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://localhost:9999")
+                .build();
+
+        MockResponse mockResponse = new MockResponse()
+                .setResponseCode(HttpStatus.BAD_REQUEST.value())
+                .setBody("bad request");
+
+        mockWebServer.enqueue(mockResponse);
+
+
+
+        Mono<String> responseBody = webClient.get().retrieve().bodyToMono(String.class);
+        try {
+            // 400, 500에러는 자동으로 WebClientResponseException 발생
+            System.out.println(responseBody.block());
+            System.out.println("No Exception");
+
+        } catch (WebClientResponseException e) {
+            System.out.println("Exception run!!!");
+        }
+
+    }
+
+    @Test
+    void callMockSeverIf() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://localhost:9999")
+                .build();
+
+//        MockResponse mockResponse = new MockResponse()
+//                .setResponseCode(HttpStatus.BAD_REQUEST.value())
+//                .setBody("bad request");
+
+        MockResponse mockResponse = new MockResponse()
+                .setResponseCode(HttpStatus.OK.value())
+                .setBody("100");
+
+        mockWebServer.enqueue(mockResponse);
+
+        Mono<Object> ret = webClient.get().exchangeToMono(clientResponse -> {
+            if (clientResponse.statusCode().isError()) {
+                System.out.println("ErrorCode");
+                return clientResponse.bodyToMono(String.class);
+            } else{
+                System.out.println("Ok Code");
+                return clientResponse.bodyToMono(Integer.class);
+            }
+        });
+
+        System.out.println(ret.block());
+    }
 }
+
