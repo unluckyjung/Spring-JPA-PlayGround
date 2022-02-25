9bd2a798ffc75a9833c6c465d214b7595b2f38c5
unluckyjung <ybook2012@gmail.com>
2022-02-25 21:11:21 +0900

    feat: optional orElse 사용시 주의할점 정리

--- play-ground/src/test/java/com/example/playground/domain/repository/MemberRepositoryTest.java
+++ play-ground/src/test/java/com/example/playground/domain/repository/MemberRepositoryTest.java
@@ -1,26 +1,42 @@
 package com.example.playground.domain.repository;
 
 import com.example.playground.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Slf4j
 class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
 
-    @Test
-    void memberSaveTest() {
-        Member member = new Member("jys");
-        Member savedMember = memberRepository.save(member);
+    @BeforeEach
+    void setUp() {
+        memberRepository.save(new Member("unluckyjung"));
+    }
 
-        Optional<Member> loadMember = memberRepository.findById(savedMember.getId());
-        assertThat(savedMember).isEqualTo(loadMember.get());
+    @Test
+    void memberGetTest() {
+        Member member = getMember("unluckyjung");
+
+        //..추가적인 비즈니스 로직..
+
+        assertThat(memberRepository.findAll().size()).isEqualTo(1);
+    }
+
+    private Member getMember(final String name) {
+        return memberRepository.findByName(name).orElse(saveMember(name));  // fail case
+//        return memberRepository.findByName(name).orElseGet(() -> saveMember(name));   // success case
+    }
+
+    private Member saveMember(final String name) {
+        log.info("saveMember run!!");
+        return memberRepository.save(new Member(name));
     }
 }
